12d01d3561beb8c73ca2fee4e3219d5607da5a18
Chase Horner <chase.horner@ku.edu>
2023-02-10 14:19:06 -0600

    Commit fixing Program1

--- Cprogram1/Program1_chaseHorner.c
+++ Cprogram1/Program1_chaseHorner.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Prints sales and months
+void print_sales(double *sales_array){
+  char *months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+  printf("Monthly sales report for 2022:\n");
+  printf("  Month       Sales\n");
+  for(int k = 0; k < 12; k++){
+    printf("%-10s  $%.2f\n", months[k], sales_array[k]);}
+  }
 
 
+//Prints min, max, and mean
 void min_max_mean(double *sales_array){
   char months[12][12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
   int min, max;
@@ -13,11 +24,13 @@ void min_max_mean(double *sales_array){
     if (sales_array[i] > sales_array[max]){
       max = i;}
   }
-  printf("Minimum sales: $%.2f (%s)\n", sales_array[min], months[min]);
+  printf("\nSales summary:");
+  printf("\nMinimum sales: $%.2f (%s)\n", sales_array[min], months[min]);
   printf("Maximum sales: $%.2f (%s)\n", sales_array[max], months[max]);
   printf("Average sales: $%.2f\n", sum/12);
 }
 
+//Prints moviving averages each 6 months
 void moving_mean(double *sales_array){
   char months[12][12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
   printf("\nSix-Month Moving Average Report:\n");
@@ -27,6 +40,7 @@ void moving_mean(double *sales_array){
   }
 }
 
+//Prints the sales in desceding order
 void descending(double *sales_array){
   char *months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
   printf("\nSales report (Highest to Lowest):\n");
@@ -53,7 +67,31 @@ void descending(double *sales_array){
 
 
 int main(){
-  double sales_array[12] = {23458.01, 40112.00, 56011.85, 37820.88, 37904.67, 60200.22, 72400.31, 56210.89, 67230.84, 68233.12, 80950.34, 95225.22};
+//double sales_array[12] = {23458.01, 40112.00, 56011.85, 37820.88, 37904.67, 60200.22, 72400.31, 56210.89, 67230.84, 68233.12, 80950.34, 95225.22};
+
+//Opens, reads, and puts the content of a file into an sales_array
+FILE *fp = fopen("input.txt", "r");
+  if (fp == NULL) {
+      printf("Error opening file!\n");
+      return 1;
+  }
+
+  int count = 0;
+  double num;
+  while (fscanf(fp, "%lf", &num) != EOF) {
+      count++;
+  }
+  rewind(fp);
+
+  double sales_array[count];
+  int i = 0;
+  while (fscanf(fp, "%lf", &sales_array[i]) != EOF) {
+      i++;
+  }
+  fclose(fp);
+
+// calls each function to complete the problem
+  print_sales(sales_array);
   min_max_mean(sales_array);
   moving_mean(sales_array);
   descending(sales_array);
